207a067c022cb372170d7f117063cfd21a1b5439
Maher Dimassi <150962680+CryptoMaher@users.noreply.github.com>
2024-01-14 15:22:20 +0100

    Add files via upload

---
+++ Python/fundementals/fundamentals/for_loop_basic1/for_loop_basic1.py
@@ -0,0 +1,29 @@
+for i in range(0,151):
+    print(i)
+
+for i in range(5,1001.5):
+        print(i)
+
+for i in range(0,101):
+    if i % 10 == 0:
+        print("coding dojo")
+        elif i % 5 == 0:
+            print("coding")
+        else:
+            print(1)
+
+sum = 0
+for i in range(0,5000001,2):
+    sum += i 
+print(sum)
+
+for i in range(2018,0,-4):
+    print(i)
+
+low = 2
+high = 9 
+mult = 3
+
+for i in range(low,high + 1):
+    if i % mult == 0:
+        print(i)
---
+++ Python/week 1/practice/hello-world.py
@@ -0,0 +1,14 @@
+print( "Hello World" )
+
+name = "Noelle"
+print( "Hello", name,"!" )
+print( "Hello " + name + "!" )
+
+name = 42
+print("Hello" , 42, "!" )
+print( "Hello " + str(42) + "!")
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "I love to eat {} and {}.".format(fave_food1,fave_food2) )
+print( f"I love to eat {fave_food1} and {fave_food2}." )
